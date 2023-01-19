42dfa31a3fee0160c249b026a580b6407651217c
Gabriel <gxguinho@gmail.com>
2023-01-18 22:51:04 -0300

    add rote habits and get day

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,16 @@
-import fasify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import fasify from "fastify";
+import { appRoutes } from "./routes";
 
 const app = fasify();
-const prisma = new PrismaClient();
 
 app.register(cors);
+app.register(appRoutes);
 
-app.get("/hello", () => {
-  const habits = prisma.habit.findMany();
-
-  return habits;
-});
-
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log("HTTP Server running");
-});
+app
+  .listen({
+    port: 3333
+  })
+  .then(() => {
+    console.log("HTTP Server running");
+  });
