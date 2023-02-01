46f1e82968fe026e32acfe79b4fa0cf2145dfe9f
Gabriel <gxguinho@gmail.com>
2023-02-01 00:39:48 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,9 @@ app.register(appRoutes);
 
 app
   .listen({
-    port: 3333
+    port: 3333,
+    //host: "192.168.100.41",
+    host: "0.0.0.0"
   })
   .then(() => {
     console.log("HTTP Server running");
